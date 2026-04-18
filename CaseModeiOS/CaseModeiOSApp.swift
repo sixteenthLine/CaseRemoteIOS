0c7de86baecf13633ebc81cb717894fa118c1ee7
dolomone <go67xey@mytum.de>
2026-04-18 19:45:34 +0200

    Initial Commit

---
+++ CaseModeiOS/CaseModeiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseModeiOSApp.swift
+//  CaseModeiOS
+//
+//  Created by Mykhailo Surovennyi on 18.04.26.
+//
+
+import SwiftUI
+
+@main
+struct CaseModeiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
